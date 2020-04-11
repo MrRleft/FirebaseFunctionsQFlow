36d9a1049d1be36260e5bf3668b46778795ccfb1
Victor <vigomezj@ucm.es>
2020-04-11 22:47:51 +0200

    Try this

--- functions/index.js
+++ functions/index.js
@@ -4,19 +4,26 @@ const shortid = require('shortid');
 admin.initializeApp();
 
 exports.addQueue = functions.https.onCall((data, context) => {
-    
-    return admin.database().ref("/queue").push({
-        business_associated: data.business_associated,
-        capacity: data.capacity,
-        date_created: data.date_created,
-        date_finished: data.date_finished,
-        description: data.description,
-        is_locked: data.is_locked,
-        name: data.is_locked,
-        join_id: shortid.generate() 
+    var business_associated = data.business_associated;
+    var capacity = data.capacity;
+    var date_created = data.date_created;
+    var ate_finished = data.date_finished;
+    var description = data.description;
+    var is_locked = data.is_locked;
+    var name = data.is_locked;
+    var join_id = shortid.generate(); 
+    return admin.database().ref('/queue').push({
+        business_associated: business_associated,
+        capacity: capacity,
+        date_created: date_created,
+        date_finished: date_finished,
+        description: description,
+        is_locked: is_locked,
+        name: is_locked,
+        join_id: join_id 
     }).then(() => {
         console.log("New Queue added")
-        return {data, join_id: shortid.generate};
+        return {data, join_id: join_id};
     }).catch((error) => {
         throw new functions.https.HttpsError(error)
     })
